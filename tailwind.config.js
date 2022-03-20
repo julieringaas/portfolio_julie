8642da8d527d4652cf8325e390575930bb2e56b7
julieringaas <89411616+julieringaas@users.noreply.github.com>
2022-03-20 19:35:39 +0100

    commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,14 @@
 module.exports = {
   content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        "hero-pattern": "url('/src/bg.png')",
+      },
+    },
+    plugins: [
+      require("@tailwindcss/typography"),
+      require("@tailwindcss/forms"),
+    ],
   },
-  plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms")],
 };
